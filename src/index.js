dc6c0efcf766bf65d98b3428f6ecbd9a6a3fcc7d
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-08-08 23:02:37 -0300

    fix: update router initial

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App';
 import Home from './components/Home'
 import NavBar from './components/NavBar'
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-   
-   <Route path='/Home' render={() => <Home />} />
-   <Route>
-    <NavBar />
-    <Route path='/randomRecipes' render={() => <App />} />
-   </Route>
-    
+    <Route path='/' render={() => <Home />} />
+    <Route>
+      <NavBar />
+      <Route path='/randomRecipes' render={() => <App />} />
+    </Route>
   </BrowserRouter>,
   document.getElementById('root')
 );
